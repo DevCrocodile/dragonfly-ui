0567013ab726e9e2b51f42eb8f6a015440ac9f46
Carlos Avila <carlosavila.dev@gmail.com>
2025-08-16 13:57:16 -0600

    Fix (Changed CardProps to extends HTMLDivElement)

--- src/components/card/card.tsx
+++ src/components/card/card.tsx
@@ -1,60 +1,58 @@
 import { cn } from '@utils/classes'
 
-export interface CardProps {
-    children: React.ReactNode,
-    className?: string,
-    onClick?: () => void;
+export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+    children: React.ReactNode
 }
 
-export function Card ({ children, className, onClick }: CardProps) {
+export function Card ({ children, className, ...props }: CardProps) {
     const baseStyles = 'df:rounded-lg df:border df:border-border df:shadow-sm df:transition-shadow'
     return (
-        <div className={cn(baseStyles, className)} onClick={onClick}>
+        <div className={cn(baseStyles, className)} {...props}>
             {children}
         </div>
     );
 }
 
-export function CardHeader ({ children, className }: CardProps) {
+export function CardHeader ({ children, className, ...props }: CardProps) {
     const baseStyles = 'df:p-6'
     return (
-        <div className={cn(baseStyles, className)}>
+        <div className={cn(baseStyles, className)} {...props}>
             {children}
         </div>
     );
 }
 
-export function CardTitle ({ children, className }: CardProps) {
+export function CardTitle ({ children, className, ...props }: CardProps) {
     const baseStyles = 'df:text-text df:text-2xl df:font-semibold'
     return (
-        <div className={cn(baseStyles, className)}>
+        <div className={cn(baseStyles, className)} {...props}>
             {children}
         </div>
     );
 }
 
-export function CardDescription ({ children, className }: CardProps) {
+export function CardDescription ({ children, className, ...props }: CardProps) {
     const baseStyles = 'df:text-muted df:text-sm df:font-normal'
     return (
-        <div className={cn(baseStyles, className)}>
+        <div className={cn(baseStyles, className)} {...props}>
             {children}
         </div>
     );
 }
 
-export function CardContent ({ children, className }: CardProps){
+export function CardContent ({ children, className, ...props }: CardProps){
     const baseStyles = 'df:p-6 df:pt-0'
     return(
-        <div className={cn(baseStyles, className)}>
+        <div className={cn(baseStyles, className)} {...props}>
             {children}
         </div>
     )
 }
 
-export function CardFooter ({ children, className }: CardProps) {
+export function CardFooter ({ children, className, ...props }: CardProps) {
     const baseStyles = 'df:p-6 df:pt-0'
     return (
-        <div className={cn(baseStyles, className)}>
+        <div className={cn(baseStyles, className)} {...props}>
             {children}
         </div>
     );

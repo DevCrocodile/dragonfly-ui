b27f0185b1111a7253f3f6dd7dfabb5a912c1496
Carlos Avila <carlosavila.dev@gmail.com>
2025-08-17 08:40:41 -0600

    Fix (Button component props changed)

--- src/components/button/button.stories.tsx
+++ src/components/button/button.stories.tsx
@@ -32,5 +32,6 @@ export const Disabled: Story = {
     args: {
         children: 'Disabled Button',
         variant: 'ghost',
+        disabled: true,
     },
 };
--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,15 +1,5 @@
 import { cn } from "@utils/classes";
 
-export interface ButtonProps{
-    children: React.ReactNode;
-    className?: string;
-    variant?: 'primary' | 'secondary' | 'ghost';
-    size?: 'small' | 'medium' | 'large';
-    type?: 'button' | 'submit' | 'reset';
-    disabled?: boolean;
-    onClick?: () => void;
-}
-
 const VARIANTS ={
     primary: 'df:bg-[var(--primary)] df:text-[var(--primary-foreground)] df:hover:bg-[var(--primary-hover)]',
     secondary: 'df:bg-[var(--secondary)] df:text-[var(--secondary-foreground)] df:hover:bg-[var(--secondary-hover)]',
@@ -22,8 +12,17 @@ const SIZES = {
     large: 'df:text-lg df:px-4 df:py-2 df:rounded-lg',
 }
 
-export function Button({ children, className = '', variant = "primary", size = "medium", type = "button", onClick = undefined, disabled = false }: ButtonProps) {
+export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+    children: React.ReactNode;
+    className?: string;
+    variant?: keyof typeof VARIANTS;
+    size?: keyof typeof SIZES;
+    onClick?: () => void;
+}
+
+
+export function Button({ children, className = '', variant = "primary", size = "medium", onClick = undefined, ...props}: ButtonProps) {
     const baseStyles = 'df:flex df:items-center df:cursor-pointer df:justify-center df:gap-2 df:transition-colors df:border df:font-medium df:px-4 df:py-2 df:disabled:pointer-events-none df:disabled:opacity-50 df:text-shadow-xs'
 
-    return <button className={cn(baseStyles, VARIANTS[variant], SIZES[size], className)} type={type} onClick={onClick} disabled={disabled}>{children}</button>;
+    return <button className={cn(baseStyles, VARIANTS[variant], SIZES[size], className)} onClick={onClick} {...props}>{children}</button>;
 }

3895bd76c4a155af40bf0883f5b0b96ba19ea508
Carlos Avila <carlosavila.dev@gmail.com>
2025-09-10 15:20:11 -0600

    Feat (Dark mode for Badge)

--- src/components/badge/badge.tsx
+++ src/components/badge/badge.tsx
@@ -4,7 +4,7 @@ const VARIANTS = {
     positive: 'df:bg-green-300 df:text-green-800 df:border-green-500',
     negative: 'df:bg-red-300 df:text-red-800 df:border-red-500',
     warning: 'df:bg-amber-300 df:text-amber-800 df:border-amber-500',
-    ghost: 'df:bg-transparent df:text-text',
+    ghost: 'df:bg-[var(--ghost)] df:text-[var(--ghost-foreground)]',
     primary: 'df:bg-[var(--primary-surface)] df:text-[var(--primary)] df:border-[var(--primary)]',
     secondary: 'df:bg-[var(--secondary-surface)] df:text-[var(--secondary)] df:border-[var(--secondary)]'
 }
@@ -15,10 +15,10 @@ export interface BadgeProps {
     variants?: keyof typeof VARIANTS;
 }
 
-export function Badge ({ children, className = '', variants = 'warning' }: BadgeProps) {
-    const baseStyles = 'df:inline-flex df:items-center df:rounded-full df:border df:border-border df:px-2.5 df:py-0.5 df:text-xs df:font-semibold df:transition-colors df:focus:outline-none df:focus:ring-2 df:text-text df:focus:ring-offset-2'
+export function Badge({ children, className = '', variants = 'warning' }: BadgeProps) {
+    const baseStyles = 'df:inline-flex df:items-center df:rounded-full df:border df:border-[var(--border)] df:px-2.5 df:py-0.5 df:text-xs df:font-semibold df:transition-colors df:text-[var(--text)]'
     return (
-        <span className={cn(baseStyles, VARIANTS[variants] ,className)}>
+        <span className={cn(baseStyles, VARIANTS[variants], className)}>
             {children}
         </span>
     );

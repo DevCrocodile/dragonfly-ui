346e92548aca2c439a87cb33802f99e36744a069
Carlos Avila <carlosavila.dev@gmail.com>
2025-09-10 15:15:37 -0600

    Feat (Dark mode for Button)

--- src/components/button/button.tsx
+++ src/components/button/button.tsx
@@ -1,9 +1,9 @@
 import { cn } from "@utils/classes";
 
-const VARIANTS ={
+const VARIANTS = {
     primary: 'df:bg-[var(--primary)] df:text-[var(--primary-foreground)] df:hover:bg-[var(--primary-hover)]',
     secondary: 'df:bg-[var(--secondary)] df:text-[var(--secondary-foreground)] df:hover:bg-[var(--secondary-hover)]',
-    ghost: 'df:bg-transparent df:hover:bg-slate-100 df:text-slate-900 df:hover:text-slate-700 df:border-border df:text-shadow-none',
+    ghost: 'df:bg-[var(--ghost)] df:hover:bg-[var(--ghost-hover)] df:text-[var(--ghost-foreground)] df:hover:text-[var(--ghost-foreground)] df:border-border df:text-shadow-none',
 }
 
 const SIZES = {
@@ -21,8 +21,8 @@ export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElemen
 }
 
 
-export function Button({ children, className = '', variant = "primary", size = "medium", onClick = undefined, ...props}: ButtonProps) {
-    const baseStyles = 'df:flex df:items-center df:cursor-pointer df:justify-center df:gap-2 df:transition-colors df:border df:font-medium df:px-4 df:py-2 df:disabled:pointer-events-none df:disabled:opacity-50 df:text-shadow-xs'
+export function Button({ children, className = '', variant = "primary", size = "medium", onClick = undefined, ...props }: ButtonProps) {
+    const baseStyles = 'df:flex df:items-center df:cursor-pointer df:justify-center df:gap-2 df:transition-colors df:border df:border-[var(--border)] df:font-medium df:px-4 df:py-2 df:disabled:pointer-events-none df:disabled:opacity-50 df:text-shadow-xs'
 
     return <button className={cn(baseStyles, VARIANTS[variant], SIZES[size], className)} onClick={onClick} {...props}>{children}</button>;
 }

b532521912318aada3aea5d2392a75a468cd922d
Carlos Avila <carlosavila.dev@gmail.com>
2025-09-10 15:39:00 -0600

    Feat (Dark mode for Progressbar)

--- src/components/progressbar/progressbar.tsx
+++ src/components/progressbar/progressbar.tsx
@@ -8,9 +8,9 @@ export interface ProgressBarProps {
 
 export function ProgressBar({ progress, className, color }: ProgressBarProps) {
     return (
-        <div className={cn("df:relative df:w-full df:h-2 df:bg-gray-200 df:rounded", className)}>
+        <div className={cn("df:relative df:w-full df:h-2 df:bg-[var(--border)] df:rounded", className)}>
             <div
-                className={cn("df:absolute df:top-0 df:left-0 df:h-full df:bg-blue-500 df:rounded-full", {
+                className={cn("df:absolute df:top-0 df:left-0 df:h-full df:rounded-full", {
                     "df:transition-all df:duration-300": progress < 100,
                 })}
                 style={{ width: `${progress.toString()}%`, backgroundColor: color }}
